67475ac28ad3a97281958cdc5df11bffc9e9030e
mohd zeeshan <mohdzeeshan7775@gamil.com>
2022-06-03 21:26:57 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles/App.css';
 import like from '../like.svg';
+
 const App = () => {
 const [count,setCount] = React.useState(0)
 const handleClick = () =>{
